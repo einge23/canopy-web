00f4edb4bac908d36c9b1c0c3923f599fb56e06b
einge23 <einge823@gmail.com>
2025-04-28 23:33:39 -0400

    try to hit api

--- src/api/api-base.ts
+++ src/api/api-base.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
-
-const baseURL = import.meta.env.VITE_CANOPY_API_URL!;
+const env = process.env.NODE_ENV || "development";
+const baseURL =
+    env === "production" ?
+        process.env.VITE_CANOPY_API_URL
+    :   "http://localhost:3000/api";
 
 // Create a function to get a configured API instance with auth
 export const api = axios.create({
